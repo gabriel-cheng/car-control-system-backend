137c76695f737009854bc71265b7c5f48e07ec0f
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-06-02 11:05:51 -0300

    Initial files

---
+++ src/main/java/com/example/carControlSystem/CarControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carControlSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carControlSystem/CarControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carControlSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
